6479a58abd7ae05fcec7ff6d02146702dd1c1043
AwesomeMinsik <beauty3579@gmail.com>
2024-03-11 18:13:42 +0900

    first commit

---
+++ toyboard/src/test/java/co/kr/toyboard/ToyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.toyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
